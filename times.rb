9404193816de3bb65290ec1c16a32de133128253
Chris Nunez <cenunez47@gmail.com>
2017-09-18 00:29:35 +0000

    Done.

--- times.rb
+++ times.rb
@@ -1,7 +1,12 @@
 def using_times
-	#your code here
+  7.times do
+    puts "Wingardium Leviosa"
+  end
 end
 
-
-
-
+# tiny_bananas = 100
+# 9.times do
+#   puts "Taking away seven tiny bananas"
+#   tiny_bananas = tiny_bananas - 9
+#   puts "Now if you've only got #{tiny_bananas} left!"
+# end
